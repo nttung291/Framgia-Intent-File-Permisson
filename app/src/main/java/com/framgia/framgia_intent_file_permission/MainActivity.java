7a0bbb97ee7bbc5656d2776acc667b4a79d40d10
nttung291 <nttung291@gmail.com>
2018-01-26 17:51:14 +0700

    Final

--- app/src/main/java/com/framgia/framgia_intent_file_permission/MainActivity.java
+++ app/src/main/java/com/framgia/framgia_intent_file_permission/MainActivity.java
@@ -1,7 +1,22 @@
 package com.framgia.framgia_intent_file_permission;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +24,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPer();
+        setUI();
     }
+
+    private void setUI() {
+        RecyclerView recyclerView = findViewById(R.id.recylerview);
+        File sdDir = Environment
+                .getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File[] files = sdDir.listFiles();
+        File[] imageFiles = null;
+        for (File file : files) {
+            if (file.getName().equals("Camera")) {
+                imageFiles = file.listFiles();
+                Log.d("Files", "FileName:" + file.getName() + " /// " + file.getAbsolutePath());
+                break;
+            }
+        }
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false);
+        gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+            @Override
+            public int getSpanSize(int position) {
+                return 1;
+            }
+        });
+        recyclerView.setLayoutManager(gridLayoutManager);
+        RecyclerAdapter adapter = new RecyclerAdapter(imageFiles,this);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private void requestPer() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if ((checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                            != PackageManager.PERMISSION_GRANTED)) {
+                ActivityCompat.requestPermissions
+                        (this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/framgia/framgia_intent_file_permission/RecyclerAdapter.java
@@ -0,0 +1,59 @@
+package com.framgia.framgia_intent_file_permission;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.io.File;
+
+/**
+ * Created by nttungg on 1/26/18.
+ */
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private LayoutInflater layoutInflater;
+    private File[] files;
+    private Context context;
+
+    public RecyclerAdapter(File[] files, Context context) {
+        this.files = files;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (layoutInflater == null){
+            layoutInflater = LayoutInflater.from(parent.getContext());
+        }
+        View itemview = layoutInflater.inflate(R.layout.item_recyclerview,parent,false);
+        return new ViewHolder(itemview);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.bindData(files[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return files.length;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView imageView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_view);
+        }
+        public void bindData(File file){
+
+            Glide.with(context).load(file).into(imageView);
+        }
+
+    }
+}
